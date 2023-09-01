ca078851be545a227f3cc1be280b4c89b2d8bd5d
Zheka Chakhov <zhekachakhov@mail.ru>
2023-09-01 19:24:56 +0300

    made header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./index.scss";
+import icon from "./assets/icon.svg";
 
 function App() {
 	return (
-		<>
-			<div>blablabla</div>
-		</>
+		<div className="App">
+			<div className="header">
+				<div className="title">ПОКЕМОНЫ API</div>
+				<div className="info">
+					<img src={icon} />
+					<div className="text">Нажмите на нужного Покемона</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
