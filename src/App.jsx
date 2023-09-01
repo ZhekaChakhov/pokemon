af272fde945fcc0e86f1340b093be2026ddea416
Zheka Chakhov <zhekachakhov@mail.ru>
2023-09-02 00:43:44 +0300

    added chips

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.scss";
 import icon from "./assets/icon.svg";
+import Chip from "@mui/material/Chip";
 
 function App() {
 	return (
@@ -11,6 +12,21 @@ function App() {
 					<img src={icon} />
 					<div className="text">Нажмите на нужного Покемона</div>
 				</div>
+			</div>
+			<div className="main">
+				<div className="chips">
+					<div className="upper-chips">
+						<Chip label="bulbasaur" className="Chip" />
+						<Chip label="ivysaur" className="Chip" />
+						<Chip label="venusaur" className="Chip" />
+					</div>
+					<div className="lower-chips">
+						<Chip label="charmander" className="Chip" />
+						<Chip label="charizard" className="Chip" />
+						<Chip label="weedle" className="Chip" />
+					</div>
+				</div>
+				<div className="def"></div>
 			</div>
 		</div>
 	);
